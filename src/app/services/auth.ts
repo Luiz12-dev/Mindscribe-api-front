6c81c2dd5d14debdfed89f6e6759c9e512a1fd38
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-01-21 11:03:54 -0300

    feat(service): Adiciona metodo de login

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private apiUrl = 'https://localhost:8080/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(Credentials: any) {
+    return this.http.post<any>(`${this.apiUrl}/login`, Credentials).pipe(
+      tap((response) => {
+        if (response.token) {
+          localStorage.setItem('auth_token', response.token);
+        }
+      }),
+    );
+  }
 }
