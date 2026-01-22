fb4107bd4b577441fc3fd1bc1fe07c2bb4d9a4a4
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-01-22 15:17:33 -0300

    feat: Adiciona a funcionalidade {onLogin}

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  email = '';
+  password = '';
 
+  constructor(
+    private auth: Auth,
+    private router: Router,
+  ) {}
+
+  onLogin() {
+    const dadosLogin = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.auth.login(dadosLogin).subscribe({
+      next: (res) => {
+        console.log('Resposta do Backend:', res);
+        alert('Login bem-sucedido!');
+      },
+      error: (err) => {
+        console.error('Erro ao fazer login:', err);
+        alert('Falha no login. Verifique suas credenciais.');
+      },
+    });
+  }
 }
