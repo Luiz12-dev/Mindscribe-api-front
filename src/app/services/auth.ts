e9c5a994957388b36782b22825ef322ae30b684f
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-02-12 11:04:07 -0300

    feat(service): Cria metodo register e adiciona a url correspondente

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -6,12 +6,18 @@ import { tap } from 'rxjs';
   providedIn: 'root',
 })
 export class Auth {
-  private apiUrl = 'http://localhost:8080/auth';
+  private loginUrl = 'http://localhost:8080/auth/login';
+
+  private registerUrl = 'http://localhost:8080/users';
 
   constructor(private http: HttpClient) {}
 
+  register(Credentials: any) {
+    return this.http.post<any>(this.registerUrl, Credentials);
+  }
+
   login(Credentials: any) {
-    return this.http.post<any>(`${this.apiUrl}/login`, Credentials).pipe(
+    return this.http.post<any>(this.loginUrl, Credentials).pipe(
       tap((response) => {
         if (response.token) {
           localStorage.setItem('auth_token', response.token);
