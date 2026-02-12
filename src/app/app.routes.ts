923a9dfba9d710864b06e51ad2ca631da3e8c4a2
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-02-12 11:08:36 -0300

    feat(app): Adiciona rota dos compoenentes criados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [{ path: '', component: Login }];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard },
+];
