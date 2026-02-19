13ecc97822d1a25d1d66dbaebaaa7a708530d49e
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-02-19 14:30:50 -0300

    refactor(pages/login): Otimiza lógica do método onLogin()

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,37 +2,57 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../../services/auth';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  email = '';
-  password = '';
+  credentials = {
+    email: '',
+    password: '',
+  };
+
+  errorMessage: string = '';
 
   constructor(
     private auth: Auth,
     private router: Router,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   onLogin() {
-    const dadosLogin = {
-      email: this.email,
-      password: this.password,
-    };
+    this.errorMessage = '';
 
-    this.auth.login(dadosLogin).subscribe({
-      next: (res) => {
-        console.log('Resposta do Backend:', res);
-        alert('Login bem-sucedido!');
+    if (!this.credentials.email || !this.credentials.password) {
+      this.errorMessage = 'Porfavor preencha todos os dados.';
+      return;
+    }
+
+    this.auth.login(this.credentials).subscribe({
+      next: () => {
+        console.log('Login realizado com sucesso !');
+        this.router.navigate(['/dashboard']);
       },
-      error: (err) => {
-        console.error('Erro ao fazer login:', err);
-        alert('Falha no login. Verifique suas credenciais.');
+      error: (err: HttpErrorResponse) => {
+        console.error('Erro ao fazer login: ', err);
+
+        if (err.status === 400) {
+          // Erro de validação
+          this.errorMessage = 'Email ou senha em formato inválido.';
+        } else if (err.status === 401) {
+          // Erro de autenticação
+          this.errorMessage = 'Email ou senha incorretos.';
+        } else {
+          this.errorMessage = 'Erro ao fazer login. Tente novamente.';
+        }
+
+        this.cdr.detectChanges();
       },
     });
   }
