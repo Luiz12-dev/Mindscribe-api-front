86ca59b35c29fa65ec5e974fcf2ef4e018aecf74
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-02-27 15:05:16 -0300

    fix(service): Corrige nomeclatura e limpa o código

--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -1,19 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  // armazena o token de autenticação do localStorage
-  const token = localStorage.getItem('auth_Token');
+  const token = localStorage.getItem('auth_token');
 
-  // se o token existir, clona a requisição e adiciona o cabeçalho Authorization
   if (token) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
 
-    //  encaminha a requisição clonada com o cabeçalho adicionado
     return next(authReq);
   }
 
-  // se o token não existir, encaminha a requisição original
   return next(req);
 };
