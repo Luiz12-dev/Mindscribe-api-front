145b45fde2c2b9e7ddc89385c1989f4258be6676
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-04-06 12:07:13 -0300

    refactor: remove component header e arquivos associados

--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -3,7 +3,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth_token');
 
-  // 📸 CÂMERA 1: O porteiro achou o token no bolso?
   console.log('INTERCEPTOR RODOU! Token encontrado:', token);
 
   if (token) {
@@ -11,7 +10,6 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
 
-    // 📸 CÂMERA 2: O porteiro enviou a requisição?
     console.log('INTERCEPTOR: Crachá anexado e requisição enviada!');
     return next(authReq);
   }
